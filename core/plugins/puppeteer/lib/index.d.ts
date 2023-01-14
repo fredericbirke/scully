a9c1e199581bfc5991316e29eea79e95535fd602
sanderelias <sanderelias+git@gmail.com>
2023-01-14 12:06:09 +0100

    fix(plugins): :bug: ppt plugin, fix bug whit eventName confict

--- core/plugins/puppeteer/lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare global {
-    interface Window {
-        ScullyIO: string | undefined;
-        scullyVersion: string | undefined;
-        "ScullyIO-injected": string | undefined;
-        onCustomEvent: (...args: any[]) => unknown;
-    }
-}
-export {};
--- core/plugins/puppeteer/lib/lib/plugins-scully-plugin-puppeteer.d.ts
+++
@@ -1,2 +0,0 @@
-import { HandledRoute } from '@scullyio/scully';
-export declare const puppeteerRender: (route: HandledRoute) => Promise<string>;
